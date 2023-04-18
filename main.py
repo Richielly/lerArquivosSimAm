3c8574fdeb4f391953a03ab9b8e8172c7a97672c
Richielly <richielly.positivo@gmail.com>
2023-04-18 15:31:29 -0300

    V_0.0.1

--- main.py
+++ main.py
@@ -23,17 +23,32 @@ result_2 = pd.DataFrame(df_2)
 
 file_name = "sqlUpdate_Hodometro_Horimetro.txt"
 
-for linha in result.values:
-#     sql = f"select * from SCP55_BEM where codbem = '{linha[0]}' union all"
-    sql = f""" update SCF_VEICULOCONTROLESIMAM cs set cs.VLRACUMULADORFINAL = {linha[4]} where cs.IDVEICULOCONTROLESIMAM = (select cs.IDVEICULOCONTROLESIMAM from SCF_VEICULOCONTROLESIMAM cs
-                join SCF_VEICULO v on (cs.IDVEICULO = v.IDVEICULO)
-                join scp55_bem b on (b.idbem = v.idbem)
-                where
-                cs.dtlancamento between '27.12.2022' and '27.01.2023' and
-                v.CODENTIDADE = {codEntidade} and 
-                b.codbem = '{linha[0]}'); """
+def data_inicial():
+    for linha in result.values:
+    #     sql = f"select * from SCP55_BEM where codbem = '{linha[0]}' union all"
+        sql = f""" update SCF_VEICULOCONTROLESIMAM cs set cs.VLRACUMULADORINICIAL = {linha[4]} where cs.IDVEICULOCONTROLESIMAM = (select cs.IDVEICULOCONTROLESIMAM from SCF_VEICULOCONTROLESIMAM cs
+                    join SCF_VEICULO v on (cs.IDVEICULO = v.IDVEICULO)
+                    join scp55_bem b on (b.idbem = v.idbem)
+                    where
+                    cs.dtlancamento between '01.01.2023' and '01.02.2023' and
+                    v.CODENTIDADE = {codEntidade} and 
+                    b.codbem = '{linha[0]}'); """
 
-    with open(file_name, 'a', encoding='utf-8') as file:
-        file.write(f'\n{sql}')
+        with open('Inicial'+file_name, 'a', encoding='utf-8') as file:
+            file.write(f'\n{sql}')
 
 
+def data_final():
+    for linha in result.values:
+        sql = f""" update SCF_VEICULOCONTROLESIMAM cs set cs.VLRACUMULADORFINAL = {linha[4]} where cs.IDVEICULOCONTROLESIMAM = (select cs.IDVEICULOCONTROLESIMAM from SCF_VEICULOCONTROLESIMAM cs
+                    join SCF_VEICULO v on (cs.IDVEICULO = v.IDVEICULO)
+                    join scp55_bem b on (b.idbem = v.idbem)
+                    where
+                    cs.dtlancamento between '27.12.2022' and '27.01.2023' and
+                    v.CODENTIDADE = {codEntidade} and 
+                    b.codbem = '{linha[0]}'); """
+
+        with open('Final'+file_name, 'a', encoding='utf-8') as file:
+            file.write(f'\n{sql}')
+
+data_inicial()
